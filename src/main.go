84d473e76ebb8392aac1007cfa98ca830c107998
lgramu97 <gramuglialautaro@gmail.com>
2021-04-10 20:50:54 -0300

    Uso de if,else, y condiciones

--- src/main.go
+++ src/main.go
@@ -2,67 +2,51 @@ package main
 
 import "fmt"
 
+func isPair(number int) bool {
+	return (number % 2) == 0
+}
+
+func parOimpar(number int) {
+	if isPair(number) {
+		fmt.Printf("El numero %d es un numero par \n", number)
+	} else {
+		fmt.Printf("El numero %d es un numero impar \n", number)
+	}
+}
+
+func validateUser(userName, password string) bool {
+	return (userName == password)
+}
+
 func main() {
 
-	// for condicional
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	valor1 := 1
+	valor2 := 2
+
+	if valor1 == 1 {
+		fmt.Printf("Se cumple la condicion el valor es : %d \n", valor1)
+	} else {
+		fmt.Printf("Se cumple la condicion el valor es : %d \n", valor2)
 	}
 
-	// for while (for hasta que se cumpla una condicion)
-	counter := 0
-	for counter < 10 {
-		fmt.Println(counter)
-		counter++
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
 	}
 
-	// for forever (ciclo infinito)
-	counterForEver := 0
-	for {
-		fmt.Println("COunter forever", counterForEver)
-		counterForEver++
-		if counterForEver == 1000 {
-			break
-		}
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
 	}
 
-	// for descendente
-	for i := counterForEver; i > 0; i-- {
-		fmt.Println("Decremento", i)
+	if num := -2; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
 	}
 
-	// for range.
-	//Here we use range to sum the numbers in a slice. Arrays work like this too.
-	nums := []int{2, 3, 4}
-	sum := 0
-	for _, num := range nums {
-		sum += num
-	}
-	fmt.Println("sum:", sum)
-
-	//range on arrays and slices provides both the index and value for each entry.
-	//Above we didn’t need the index, so we ignored it with the blank identifier _.
-	//Sometimes we actually want the indexes though.
-	for i, num := range nums {
-		if num == 3 {
-			fmt.Println("index:", i)
-		}
-	}
-
-	//range on map iterates over key/value pairs.
-	kvs := map[string]string{"a": "apple", "b": "banana"}
-	for k, v := range kvs {
-		fmt.Printf("%s -> %s\n", k, v)
-	}
-
-	//range can also iterate over just the keys of a map.
-	for k := range kvs {
-		fmt.Println("key:", k)
-	}
-
-	//range on strings iterates over Unicode code points.
-	//The first value is the starting byte index of the rune and the second the rune itself.
-	for i, c := range "go" {
-		fmt.Println(i, c)
-	}
+	numeroCheck := 15
+	parOimpar(numeroCheck)
 }
