c27fd4c005a77c1932a7c66e587a986c54f1350c
lgramu97 <gramuglialautaro@gmail.com>
2021-04-10 21:07:16 -0300

    Uso de keywords continue, break, defer

--- src/main.go
+++ src/main.go
@@ -1,62 +1,29 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
+	// Defer (linea que se ejecuta ultimo)
+	// utilizado para cerrar conexiones, archivos, base de datos.
+	// por ejemplo defer closeFile()
+	defer fmt.Println("Hola")
+	fmt.Println("Mundo")
 
-	// Definicion y parseo de variable
-	switch modulo := 4 % 2; modulo {
-	case 0:
-		fmt.Println("Es par")
-	default:
-		fmt.Println("Es impar")
-	}
+	//Continue y break
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
 
-	// Switch sin condicion (cuando quiero anidar multiples condiciones)
-	value := 54
-	switch {
-	case value > 100:
-		fmt.Println("Es mayor que 100")
-	case value < 0:
-		fmt.Println("Es menor que 0")
-	default:
-		fmt.Println("No condicion")
-	}
-
-	switch time.Now().Weekday() {
-	case time.Saturday, time.Sunday:
-		fmt.Println("It's the weekend")
-	default:
-		fmt.Println("It's a weekday")
-	}
-
-	//switch without an expression is an alternate way to express if/else logic.
-	//Here we also show how the case expressions can be non-constants.
-	t := time.Now()
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("It's before noon")
-	default:
-		fmt.Println("It's after noon")
-	}
-
-	//A type switch compares types instead of values.
-	//You can use this to discover the type of an interface value.
-	//In this example, the variable t will have the type corresponding to its clause.
-	whatAmI := func(i interface{}) {
-		switch t := i.(type) {
-		case bool:
-			fmt.Println("I'm a bool")
-		case int:
-			fmt.Println("I'm an int")
-		default:
-			fmt.Printf("Don't know type %T\n", t)
+		// Continue
+		if i == 2 {
+			fmt.Println("Es 2")
+			continue // A pesar de un error quiero que continue por ej.
 		}
+
+		// Break
+		if i >= 8 {
+			fmt.Println("Muy alto el numero")
+			break // Quiero que corte el for.
+		}
+
 	}
-	whatAmI(true)
-	whatAmI(1)
-	whatAmI("hey")
 }
