6d735952fb6d3af639b1ccdd058ed17c2203c5f3
lgramu97 <gramuglialautaro@gmail.com>
2021-04-10 22:00:47 -0300

    Range in Slices, Palindromo, Strings

--- src/main.go
+++ src/main.go
@@ -1,53 +1,93 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func main() {
-	// ARRAY INMUTABLE, SLICE MUTABLE
-
-	// Array
-	var array [4]int // inicializar en 0 arreglo
-	array[0] = 1
-	array[1] = 2
-	fmt.Println(array, "Longitud:", len(array), "Maxima capacidad:", cap(array))
-
-	// Slice (arreglo pero no se indica la capacidad, es una lista)
-	slice := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println(slice, "Longitud:", len(slice), "Maxima capacidad:", cap(slice))
-
-	// Metodos Slice
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])  // Numero despues de : es exlusivo
-	fmt.Println(slice[2:4]) // inclusivo : exclusivo
-	fmt.Println(slice[4:])
-	fmt.Println(slice[:])
-
-	// Append 1 elemento
-	slice = append(slice, 7)
-	fmt.Println(slice[:])
-
-	// Append multiples elementos
-	newSlice := []int{8, 9, 10, 11}
-	slice = append(slice, newSlice...)
-	fmt.Println(slice[:])
-
-	b := [5]int{1, 2, 3, 4, 5}
-	fmt.Println("dcl:", b)
-	//Array types are one-dimensional, but you can compose types to build multi-dimensional data structures.
-	var twoD [2][3]int
-	for i := 0; i < 2; i++ {
-		for j := 0; j < 3; j++ {
-			twoD[i][j] = i + j
+func isPalindromo(text string) bool {
+	// version 1 comparando 1 a 1
+	cumple := true
+	text = strings.ToLower(text)
+	for i := range text {
+		if text[i] == text[len(text)-i-1] {
+			continue
+		} else {
+			cumple = false
+			break
 		}
 	}
-	fmt.Println("2d: ", twoD)
+	return cumple
+}
 
-	// Copy slices
+func isPalindromo2(text string) bool {
+	// version 2 contruyendo un nuevo string y luego comparando
+	var textReverse string
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i])
+	}
 
-	//Slices can also be copy’d. Here we create an empty slice c of the same length as slice
-	//and copy into c from s.
-	c := make([]int, len(slice))
-	copy(c, slice)
-	fmt.Println("cpy of slice:", c)
+	return strings.ToLower(textReverse) == strings.ToLower(text)
+}
+
+func main() {
+	// range iterates over elements in a variety of data structures.
+	//Let’s see how to use range with some of the data structures we’ve already learned.
+
+	//Here we use range to sum the numbers in a slice. Arrays work like this too.
+	nums := []int{2, 3, 4}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Println("sum:", sum)
+
+	//range on arrays and slices provides both the index and value for each entry.
+	// Above we didn’t need the index, so we ignored it with the blank identifier _.
+	// Sometimes we actually want the indexes though.
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
+	}
+
+	//range on map iterates over key/value pairs.
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	//range can also iterate over just the keys of a map.
+	for k := range kvs {
+		fmt.Println("key:", k)
+	}
+
+	//range on strings iterates over Unicode code points.
+	// The first value is the starting byte index of the rune and the second the rune itself.
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
+
+	//*************************************************Platzi**********************************//
+	slice := []string{"hola", "mundo", "chau"}
+
+	// Las 2 variables
+	for i, valor := range slice {
+		fmt.Println(i, valor)
+	}
+
+	// Solo la palabra
+	for _, valor := range slice {
+		fmt.Println(valor)
+	}
+
+	// Solo el indice
+	for i := range slice {
+		fmt.Println(i)
+	}
+
+	palindromo := "neuqueN"
+	fmt.Printf("Es un palindromo la palabra %s: %t \n", palindromo, isPalindromo(palindromo))
+	fmt.Printf("Es un palindromo la palabra %s: %t \n", palindromo, isPalindromo2(palindromo))
 
 }
