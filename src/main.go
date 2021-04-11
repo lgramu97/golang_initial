065d781038e05ce333ae05dee9853a18c888475a
lgramu97 <gramuglialautaro@gmail.com>
2021-04-10 22:26:32 -0300

    Structs, no existen clases pero si estructuras

--- src/main.go
+++ src/main.go
@@ -2,28 +2,62 @@ package main
 
 import "fmt"
 
+type car struct {
+	marca string
+	año   int
+}
+
+//This person struct type has name and age fields.
+type person struct {
+	name string
+	age  int
+}
+
+//newPerson constructs a new person struct with the given name.
+func newPerson(name string) *person {
+	//You can safely return a pointer to local variable as a local variable will survive
+	//the scope of the function.
+	p := person{name: name}
+	p.age = 42
+	return &p
+}
+
 func main() {
-	// Diccionarios o Maps
+	//Go’s structs are typed collections of fields.
+	//They’re useful for grouping data together to form records.
+	auto1 := car{marca: "Ford", año: 1960}
+	fmt.Println(auto1.año, auto1.marca, auto1)
 
-	// Crear map
-	edades := make(map[string]int)
-	// Agregar datos
-	edades["Jose"] = 14
-	edades["Pepe"] = 20
-	// Imprimir
-	fmt.Println(edades)
+	// Otra forma de instanciar struct
+	var auto2 car
+	auto2.marca = "Ferrari"
+	fmt.Println(auto2.año, auto2.marca, auto2) // valor por defecto en año se vuelve 0.
 
-	// Recorrer map
-	for k, value := range edades {
-		fmt.Println(k, value)
-	}
+	//This syntax creates a new struct.
+	fmt.Println(person{"Bob", 20})
 
-	// MUCHO CUIDADO!!
-	// Si la clave no existe, se muestra el valor del zero_value
-	value, ok := edades["Josee"] // Si pongo 2 valores, el segundo valor retornado es si existe.
-	fmt.Println(value, ok)
+	//	You can name the fields when initializing a struct.
+	fmt.Println(person{name: "Alice", age: 30})
 
-	value, ok = edades["Jose"]
-	fmt.Println(value, ok)
+	//	Omitted fields will be zero-valued.
+	fmt.Println(person{name: "Fred"})
+
+	//  An & prefix yields a pointer to the struct.
+	fmt.Println(&person{name: "Ann", age: 40})
+
+	//	It’s idiomatic to encapsulate new struct creation in constructor functions
+	fmt.Println(newPerson("Jon"))
+
+	//	Access struct fields with a dot.
+	s := person{name: "Sean", age: 50}
+	fmt.Println(s.name)
+
+	//	You can also use dots with struct pointers - the pointers are automatically dereferenced.
+	sp := &s
+	fmt.Println(sp.age)
+
+	//	Structs are mutable.
+	sp.age = 51
+	fmt.Println(sp.age)
 
 }
