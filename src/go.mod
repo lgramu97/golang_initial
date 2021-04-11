08c3dac5e1ea0b7d4ede0a392ce507d948453eb8
lgramu97 <gramuglialautaro@gmail.com>
2021-04-11 01:08:15 -0300

    Modules and packages

---
+++ src/go.mod
@@ -0,0 +1,7 @@
+module main
+
+require go/src/mypackage v0.0.0
+
+replace go/src/mypackage => ../mypackage
+
+go 1.16
--- src/main.go
+++ src/main.go
@@ -1,63 +1,22 @@
 package main
 
-import "fmt"
-
-type car struct {
-	marca string
-	año   int
-}
-
-//This person struct type has name and age fields.
-type person struct {
-	name string
-	age  int
-}
-
-//newPerson constructs a new person struct with the given name.
-func newPerson(name string) *person {
-	//You can safely return a pointer to local variable as a local variable will survive
-	//the scope of the function.
-	p := person{name: name}
-	p.age = 42
-	return &p
-}
+import (
+	"fmt"
+	pk "go/src/mypackage"
+)
 
 func main() {
-	//Go’s structs are typed collections of fields.
-	//They’re useful for grouping data together to form records.
-	auto1 := car{marca: "Ford", año: 1960}
-	fmt.Println(auto1.año, auto1.marca, auto1)
+	// Importar paquete. Cuando una funcion comienza con Mayuscula, PUEDE EXPORTARSE Y UTILIZARSE EN OTROS PAQUETES.
+	var mycar = pk.CarPublic
+	mycar.Marca = "Ford"
+	fmt.Println(mycar)
 
-	// Otra forma de instanciar struct
-	var auto2 car
-	auto2.marca = "Ferrari"
-	fmt.Println(auto2.año, auto2.marca, auto2) // valor por defecto en año se vuelve 0.
+	var myCar = pk.carPrivate
+	myCar.marca = "Ford"
+	fmt.Println(myCar)
 
-	//This syntax creates a new struct.
-	fmt.Println(person{"Bob", 20})
+	pk.PrintMessage("HOLA MUNDO")
 
-	//	You can name the fields when initializing a struct.
-	fmt.Println(person{name: "Alice", age: 30})
-
-	//	Omitted fields will be zero-valued.
-	fmt.Println(person{name: "Fred"})
-
-	//  An & prefix yields a pointer to the struct.
-	fmt.Println(&person{name: "Ann", age: 40})
-
-	//	It’s idiomatic to encapsulate new struct creation in constructor functions
-	fmt.Println(newPerson("Jon"))
-
-	//	Access struct fields with a dot.
-	s := person{name: "Sean", age: 50}
-	fmt.Println(s.name)
-
-	//	You can also use dots with struct pointers - the pointers are automatically dereferenced.
-	sp := &s
-	fmt.Println(sp.age)
-
-	//	Structs are mutable.
-	sp.age = 51
-	fmt.Println(sp.age)
+	pk.printMessage("HOLA MUNDO PRIVTE")
 
 }
