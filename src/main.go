d9e6a48d915d5dba59aa3eff7fc76ae6de3c0449
lgramu97 <gramuglialautaro@gmail.com>
2021-04-10 22:17:13 -0300

    Trabajando con Maps/diccionarios

--- src/main.go
+++ src/main.go
@@ -1,93 +1,29 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func isPalindromo(text string) bool {
-	// version 1 comparando 1 a 1
-	cumple := true
-	text = strings.ToLower(text)
-	for i := range text {
-		if text[i] == text[len(text)-i-1] {
-			continue
-		} else {
-			cumple = false
-			break
-		}
-	}
-	return cumple
-}
-
-func isPalindromo2(text string) bool {
-	// version 2 contruyendo un nuevo string y luego comparando
-	var textReverse string
-	for i := len(text) - 1; i >= 0; i-- {
-		textReverse += string(text[i])
-	}
-
-	return strings.ToLower(textReverse) == strings.ToLower(text)
-}
+import "fmt"
 
 func main() {
-	// range iterates over elements in a variety of data structures.
-	//Let’s see how to use range with some of the data structures we’ve already learned.
+	// Diccionarios o Maps
 
-	//Here we use range to sum the numbers in a slice. Arrays work like this too.
-	nums := []int{2, 3, 4}
-	sum := 0
-	for _, num := range nums {
-		sum += num
-	}
-	fmt.Println("sum:", sum)
+	// Crear map
+	edades := make(map[string]int)
+	// Agregar datos
+	edades["Jose"] = 14
+	edades["Pepe"] = 20
+	// Imprimir
+	fmt.Println(edades)
 
-	//range on arrays and slices provides both the index and value for each entry.
-	// Above we didn’t need the index, so we ignored it with the blank identifier _.
-	// Sometimes we actually want the indexes though.
-	for i, num := range nums {
-		if num == 3 {
-			fmt.Println("index:", i)
-		}
+	// Recorrer map
+	for k, value := range edades {
+		fmt.Println(k, value)
 	}
 
-	//range on map iterates over key/value pairs.
-	kvs := map[string]string{"a": "apple", "b": "banana"}
-	for k, v := range kvs {
-		fmt.Printf("%s -> %s\n", k, v)
-	}
+	// MUCHO CUIDADO!!
+	// Si la clave no existe, se muestra el valor del zero_value
+	value, ok := edades["Josee"] // Si pongo 2 valores, el segundo valor retornado es si existe.
+	fmt.Println(value, ok)
 
-	//range can also iterate over just the keys of a map.
-	for k := range kvs {
-		fmt.Println("key:", k)
-	}
-
-	//range on strings iterates over Unicode code points.
-	// The first value is the starting byte index of the rune and the second the rune itself.
-	for i, c := range "go" {
-		fmt.Println(i, c)
-	}
-
-	//*************************************************Platzi**********************************//
-	slice := []string{"hola", "mundo", "chau"}
-
-	// Las 2 variables
-	for i, valor := range slice {
-		fmt.Println(i, valor)
-	}
-
-	// Solo la palabra
-	for _, valor := range slice {
-		fmt.Println(valor)
-	}
-
-	// Solo el indice
-	for i := range slice {
-		fmt.Println(i)
-	}
-
-	palindromo := "neuqueN"
-	fmt.Printf("Es un palindromo la palabra %s: %t \n", palindromo, isPalindromo(palindromo))
-	fmt.Printf("Es un palindromo la palabra %s: %t \n", palindromo, isPalindromo2(palindromo))
+	value, ok = edades["Jose"]
+	fmt.Println(value, ok)
 
 }
