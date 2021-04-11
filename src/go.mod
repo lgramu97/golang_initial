07eaeaf8897a6c2772c5b5e9de9711850c1dc1fb
lgramu97 <gramuglialautaro@gmail.com>
2021-04-11 01:14:00 -0300

    Refactor

--- src/go.mod
+++
@@ -1,7 +0,0 @@
-module main
-
-require go/src/mypackage v0.0.0
-
-replace go/src/mypackage => ../mypackage
-
-go 1.16
--- src/main.go
+++ src/main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"fmt"
-	pk "go/src/mypackage"
-)
-
 func main() {
-	// Importar paquete. Cuando una funcion comienza con Mayuscula, PUEDE EXPORTARSE Y UTILIZARSE EN OTROS PAQUETES.
-	var mycar = pk.CarPublic
-	mycar.Marca = "Ford"
-	fmt.Println(mycar)
-
-	var myCar = pk.carPrivate
-	myCar.marca = "Ford"
-	fmt.Println(myCar)
-
-	pk.PrintMessage("HOLA MUNDO")
-
-	pk.printMessage("HOLA MUNDO PRIVTE")
 
 }
